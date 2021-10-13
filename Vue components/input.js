115fa333f5d275d2ca0c17801b7a2539629a0923
describano8 <describano8@gmail.com>
2021-10-13 17:47:29 +0200

    formulari 1

---
+++ Vue components/input.js
@@ -0,0 +1,108 @@
+Vue.component('custom-input', {
+    template: '#custom-input', 
+    inheritAttrs: false,
+    data() {
+      return {
+        errors: false,
+        valid: false
+      };
+    },
+    props: {
+      label: {
+        type: String,
+        default: ''
+      },
+      value: {
+        type: [String, Number],
+        default: ''
+      },
+      type: {
+        type: String,
+        default: '',
+      }
+      
+    },
+
+    model: {
+        prop: "value",
+        event: "update"
+      },
+    methods: {
+  
+        validName: function(name) {
+          var re = /^[a-zA-Z]{6,13}$/;
+          return re.test(name);
+        },
+        isNumeric: function (n) {
+          return !isNaN(parseFloat(n)) && isFinite(n);
+        },
+        validEmail : function(email) {
+          var re = /(.+)@(.+){2,}\.(.+){2,}/;
+          return re.test(email);
+        },
+        validPassword: function(p) {
+          var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,13}$/;
+          return re.test(p);
+        },
+
+        Validation(value){
+    
+          if( this.label === "Name" ){
+              if( this.validName(value)){
+              this.errors = false;
+              this.valid = true;
+              } else {
+                console.log('wrong-name')
+                this.errors = true;
+                this.valid = false;
+              }
+          }
+
+          if( this.label === "Mobile" ){
+              if( this.isNumeric(value)){
+                this.errors = false;
+                this.valid = true;
+                  }else {
+                    console.log('wrong-mobile')
+                    this.errors = true;
+                    this.valid = false;
+                  }
+          }
+
+          if( this.label === "Postcode" ){
+              if( this.isNumeric(value)){
+                    this.errors = false;
+                    this.valid = true;
+                  }else {
+                    console.log('wrong-postcode')
+                    this.errors = true;
+                    this.valid = false;
+                  }
+          }
+
+          if( this.label === "Email" ){
+              if( this.validEmail(value)){
+                this.errors = false;
+                this.valid = true;
+                  }else {
+                    console.log('wrong-email')
+                    this.errors = true;
+                    this.valid = false;
+                  }
+          }
+
+          if( this.label === "Password" ){
+            if( this.validPassword(value)){
+              this.errors = false;
+              this.valid = true;
+                }else {
+                  console.log('wrong-password')
+                  this.errors = true;
+                  this.valid = false;
+                }
+        }
+
+          this.$emit('getvalidation', this.valid, this.label)
+    },
+    }
+  })
---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+new Vue({
+    el: '#app',
+      data() {
+        return {
+          name: "",
+          validName: false,
+          mobile: "",
+          validMobile: false,
+          postcode: "",
+          validPostcode: false,
+          email: "",
+          validPostcode: false,
+          password:"",
+          validPassword:false,
+          confirmation:"",
+          validConfirmation:false,
+          submitted: false,
+          // validated: false
+    }
+  },
+    methods: {
+        resetInput() {
+        this.name = "";
+        this.mobile = "";
+        this.postcode = "";
+        this.email = "";
+        this.password = "";
+        this.confirmation = "";
+      },
+      GetValidationMethod(valid, label){
+        // que es valid? 
+        switch(label){
+          case 'Name':
+            if(valid === true) {
+              this.validName = true
+            } 
+            break;
+          case 'Mobile':
+            if(valid === true) {
+              this.validMobile = true
+            }
+            break; 
+          case 'Postcode':
+              if(valid === true) {
+            this.validPostcode = true
+            }
+          break; 
+          case 'Email':
+            if(valid === true) {
+              this.validEmail = true
+              }
+            break;
+            case 'Password':
+              if(valid === true) {
+                this.validPassword = true
+                }
+              break;
+        }
+      },
+      SubmitForm(e){   
+        this.GetValidationMethod();
+            if( this.validName && this.validMobile && this.validPostcode && this.validEmail && this.validPassword ){
+              console.log('submitted')
+              this.submitted = true;
+              this.resetInput();
+            } else {
+              this.submitted = false;
+              e.preventDefault;
+              alert('Ups! el formulario contiene errores :(')
+            }
+      }
+    }
+})
